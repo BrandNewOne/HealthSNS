7c19a92be5119a44423b58757b2ce1cec8d95db2
BrandNewOne <rmlll@naver.com>
2023-02-15 00:35:25 +0900

    이거 모르고 안올렸네
    ㅇㅇ

--- BackEnd/service/UsersService.java
+++ BackEnd/service/UsersService.java
@@ -45,16 +45,16 @@ public class UsersService implements UserDetailsService {
         }
         //여기
 
-//        String emailInRedis = redisDao.getValues(userSignUpRequestDto.getEmail());
-//        if (Objects.isNull(emailInRedis)) {
-//            throw new BadRequestException("인증가능 시간이 초과되었습니다.");
-//        }
-//        else if(emailInRedis.equals("authEmail")){
-//            redisDao.deleteValues(userSignUpRequestDto.getEmail());
-//        }
-//        else{
-//            throw new BadRequestException("인증 번호가 다릅니다.");
-//        }
+        String emailInRedis = redisDao.getValues(userSignUpRequestDto.getEmail());
+        if (Objects.isNull(emailInRedis)) {
+            throw new BadRequestException("인증가능 시간이 초과되었습니다.");
+        }
+        else if(emailInRedis.equals("authEmail")){
+            redisDao.deleteValues(userSignUpRequestDto.getEmail());
+        }
+        else{
+            throw new BadRequestException("인증 번호가 다릅니다.");
+        }
 
         userRepository.save(User.createUser(userSignUpRequestDto, passwordEncoder));
     }
